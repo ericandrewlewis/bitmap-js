574bf11f6bdb5df0c223b372d8f012e0a63658be
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-01-27 23:58:47 -0500

    Introduce prettier

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
-var fs = require('fs')
+const fs = require("fs");
 
-function padImageData({
-  unpaddedImageData,
-  width,
-  height
-}) {
-  const colorValuesPerRow = unpaddedImageData.length / height
-  const padding = 4 - (colorValuesPerRow % 4)
-  const unpaddedRowLength = colorValuesPerRow
-  const paddedRowLength = colorValuesPerRow + padding
-  const padded = Buffer.alloc(paddedRowLength * height)
+function padImageData({ unpaddedImageData, width, height }) {
+  const colorValuesPerRow = unpaddedImageData.length / height;
+  const padding = 4 - colorValuesPerRow % 4;
+  const unpaddedRowLength = colorValuesPerRow;
+  const paddedRowLength = colorValuesPerRow + padding;
+  const padded = Buffer.alloc(paddedRowLength * height);
   for (let i = 0; i < height; i++) {
     unpaddedImageData.copy(
       padded,
       paddedRowLength * i,
       unpaddedRowLength * i,
       unpaddedRowLength * i + unpaddedRowLength
-    )
+    );
   }
-  return padded
+  return padded;
 }
 
-function bitmapFileHeader({filesize = 0, applicationHeader = 0, imageDataOffset = 0}) {
-  const buffer = Buffer.alloc(14)
+function bitmapFileHeader({
+  filesize = 0,
+  applicationHeader = 0,
+  imageDataOffset = 0
+}) {
+  const buffer = Buffer.alloc(14);
   // A bitmap file starts with a "BM" in ASCII.
-  buffer.write('B', 0)
-  buffer.write('M', 1)
+  buffer.write("B", 0);
+  buffer.write("M", 1);
   // The entire filesize.
-  buffer.writeInt32LE(filesize, 2)
+  buffer.writeInt32LE(filesize, 2);
   // 4 bytes reserved for the application creating the image.
-  buffer.writeInt32LE(applicationHeader, 6)
+  buffer.writeInt32LE(applicationHeader, 6);
   // The byte offset to access the pixel data.
-  buffer.writeInt32LE(imageDataOffset, 10)
+  buffer.writeInt32LE(imageDataOffset, 10);
   return buffer;
 }
 
 // Creates a DIB header, specifically a BITMAPINFOHEADER type
 // since it's the most widely supported.
-function dibHeader({
-  width,
-  height,
-  bitsPerPixel,
-  bitmapDataSize
-}) {
-  const buffer = Buffer.alloc(40)
+function dibHeader({ width, height, bitsPerPixel, bitmapDataSize }) {
+  const buffer = Buffer.alloc(40);
   // The size of the header.
-  buffer.writeInt32LE(40, 0)
+  buffer.writeInt32LE(40, 0);
   // The width and height of the bitmap image.
-  buffer.writeInt32LE(width, 4)
-  buffer.writeInt32LE(height, 8)
+  buffer.writeInt32LE(width, 4);
+  buffer.writeInt32LE(height, 8);
   // The number of color planes, which in bitmap files is always 1
-  buffer.writeInt16LE(1, 12)
-  buffer.writeInt16LE(bitsPerPixel, 14)
+  buffer.writeInt16LE(1, 12);
+  buffer.writeInt16LE(bitsPerPixel, 14);
 
   // Compression method, not supported in this package.
-  buffer.writeInt32LE(0, 16)
-  buffer.writeInt32LE(bitmapDataSize, 20)
+  buffer.writeInt32LE(0, 16);
+  buffer.writeInt32LE(bitmapDataSize, 20);
   // The horizontal and vertical resolution of the image.
   // On monitors: 72 DPI × 39.3701 inches per metre yields 2834.6472
-  buffer.writeInt32LE(2835, 24)
-  buffer.writeInt32LE(2835, 28)
+  buffer.writeInt32LE(2835, 24);
+  buffer.writeInt32LE(2835, 28);
   // Number of colors in the palette.
-  buffer.writeInt32LE(0, 32)
+  buffer.writeInt32LE(0, 32);
   // Number of important colors used.
-  buffer.writeInt32LE(0, 36)
+  buffer.writeInt32LE(0, 36);
   return buffer;
 }
 
@@ -76,69 +71,69 @@ function createBitmapFile({
   colorTable = Buffer.alloc(0)
 }) {
   return new Promise((resolve, reject) => {
-    const imageDataOffset = 54 + colorTable.length
-    const filesize = imageDataOffset + imageData.length
+    const imageDataOffset = 54 + colorTable.length;
+    const filesize = imageDataOffset + imageData.length;
     let fileContent = Buffer.alloc(filesize);
     let fileHeader = bitmapFileHeader({
       filesize,
       imageDataOffset
-    })
-    fileHeader.copy(fileContent)
+    });
+    fileHeader.copy(fileContent);
     dibHeader({
       width,
       height,
       bitsPerPixel,
       bitmapDataSize: imageData.length
-    }).copy(fileContent, 14)
+    }).copy(fileContent, 14);
 
-    colorTable.copy(fileContent, 54)
+    colorTable.copy(fileContent, 54);
 
-    imageData.copy(fileContent, imageDataOffset)
+    imageData.copy(fileContent, imageDataOffset);
 
-    fs.writeFile(filename, fileContent, (err) => {
-      if (err) return reject(err)
-      resolve()
-    })
-  })
+    fs.writeFile(filename, fileContent, err => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
 }
 
 function readBitmapFileHeader(filedata) {
   return {
     filesize: filedata.readInt32LE(2),
     imageDataOffset: filedata.readInt32LE(10)
-  }
+  };
 }
 
 const dibHeaderLengthToVersionMap = {
-  12: 'BITMAPCOREHEADER',
-  16: 'OS22XBITMAPHEADER',
-  40: 'BITMAPINFOHEADER',
-  52: 'BITMAPV2INFOHEADER',
-  56: 'BITMAPV3INFOHEADER',
-  64: 'OS22XBITMAPHEADER',
-  108: 'BITMAPV4HEADER',
-  124: 'BITMAPV5HEADER'
-}
+  12: "BITMAPCOREHEADER",
+  16: "OS22XBITMAPHEADER",
+  40: "BITMAPINFOHEADER",
+  52: "BITMAPV2INFOHEADER",
+  56: "BITMAPV3INFOHEADER",
+  64: "OS22XBITMAPHEADER",
+  108: "BITMAPV4HEADER",
+  124: "BITMAPV5HEADER"
+};
 
 function readDibHeader(filedata) {
-  const dibHeaderLength = filedata.readInt32LE(14)
-  const header = {}
-  header.headerLength = dibHeaderLength
-  header.headerType = dibHeaderLengthToVersionMap[dibHeaderLength]
-  header.width = filedata.readInt32LE(18)
-  header.height = filedata.readInt32LE(22)
-  if (header.headerType == 'BITMAPCOREHEADER') {
+  const dibHeaderLength = filedata.readInt32LE(14);
+  const header = {};
+  header.headerLength = dibHeaderLength;
+  header.headerType = dibHeaderLengthToVersionMap[dibHeaderLength];
+  header.width = filedata.readInt32LE(18);
+  header.height = filedata.readInt32LE(22);
+  if (header.headerType == "BITMAPCOREHEADER") {
     return header;
   }
-  header.bitsPerPixel = filedata.readInt16LE(28)
-  header.compressionType = filedata.readInt32LE(30)
-  if (header.headerType == 'OS22XBITMAPHEADER') {
+  header.bitsPerPixel = filedata.readInt16LE(28);
+  header.compressionType = filedata.readInt32LE(30);
+  if (header.headerType == "OS22XBITMAPHEADER") {
     return header;
   }
-  header.bitmapDataSize = filedata.readInt32LE(34)
-  header.numberOfColorsInPalette = filedata.readInt32LE(46)
-  header.numberOfImportantColors = filedata.readInt32LE(50)
-  if (header.headerType == 'BITMAPINFOHEADER') {
+  header.bitmapDataSize = filedata.readInt32LE(34);
+  header.numberOfColorsInPalette = filedata.readInt32LE(46);
+  header.numberOfImportantColors = filedata.readInt32LE(50);
+  if (header.headerType == "BITMAPINFOHEADER") {
     return header;
   }
   // There are more data fields in later versions of the dib header.
@@ -150,20 +145,20 @@ function readDibHeader(filedata) {
 function readBitmapFile(file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, filedata) => {
-      if (err) return reject(err)
-      const fileHeader = readBitmapFileHeader(filedata)
-      const dibHeader = readDibHeader(filedata)
-      const imageDataLength = dibHeader.bitmapDataSize
-      const imageDataOffset = fileHeader.imageDataOffset
-      const imageData = Buffer.alloc(imageDataLength)
-      filedata.copy(imageData, 0, imageDataOffset)
+      if (err) return reject(err);
+      const fileHeader = readBitmapFileHeader(filedata);
+      const dibHeader = readDibHeader(filedata);
+      const imageDataLength = dibHeader.bitmapDataSize;
+      const imageDataOffset = fileHeader.imageDataOffset;
+      const imageData = Buffer.alloc(imageDataLength);
+      filedata.copy(imageData, 0, imageDataOffset);
       resolve({
         fileHeader,
         dibHeader,
         imageData
-      })
-    })
-  })
+      });
+    });
+  });
 }
 
 module.exports = {
@@ -174,4 +169,4 @@ module.exports = {
   dibHeader,
   createBitmapFile,
   readBitmapFile
-}
+};
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-import test from 'ava';
-import m from '.';
-import fs from 'fs';
-import util from 'util';
+import test from "ava";
+import m from ".";
+import fs from "fs";
+import util from "util";
 
-const readFile = util.promisify(fs.readFile)
+const readFile = util.promisify(fs.readFile);
 
-test('converts raw bgr pixel data to padded bitmap format for 24 bits per pixel', t => {
+test("converts raw bgr pixel data to padded bitmap format for 24 bits per pixel", t => {
+  // prettier-ignore
   const unpaddedImageData = Buffer.from([
     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
@@ -14,35 +15,35 @@ test('converts raw bgr pixel data to padded bitmap format for 24 bits per pixel'
     unpaddedImageData,
     width: 2,
     height: 2
-  })
+  });
+  // prettier-ignore
   const expected = Buffer.from([
     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00,
     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00
   ])
-  t.is(0, padded.compare(expected))
-})
+  t.is(0, padded.compare(expected));
+});
 
-test('converts raw bgr pixel data to padded bitmap format for 1 bit per pixel', t => {
-  const unpaddedImageData = Buffer.from([
-    0b10000000,
-    0b01000000
-  ])
+test("converts raw bgr pixel data to padded bitmap format for 1 bit per pixel", t => {
+  const unpaddedImageData = Buffer.from([0b10000000, 0b01000000]);
   const padded = m.padImageData({
     unpaddedImageData,
     width: 2,
     height: 2
-  })
+  });
 
+  // prettier-ignore
   const expected = Buffer.from([
     0b10000000, 0x00, 0x00, 0x00,
     0b01000000, 0x00, 0x00, 0x00,
   ])
 
-  t.is(0, padded.compare(expected))
-})
+  t.is(0, padded.compare(expected));
+});
 
-test('creates a bitmap file header', t => {
-  const fileHeader = m.bitmapFileHeader({})
+test("creates a bitmap file header", t => {
+  const fileHeader = m.bitmapFileHeader({});
+  // prettier-ignore
   const expectedFileHeader = Buffer.from([
     'B'.charCodeAt(),
     'M'.charCodeAt(),
@@ -51,145 +52,131 @@ test('creates a bitmap file header', t => {
     0, 0, 0, 0
   ]);
 
-  t.is(
-    0,
-    expectedFileHeader.compare(fileHeader)
-  )
-})
+  t.is(0, expectedFileHeader.compare(fileHeader));
+});
 
-test('reads a bitmap file header', async t => {
-  const width = 1
-  const height = 1
+test("reads a bitmap file header", async t => {
+  const width = 1;
+  const height = 1;
   const imageData = m.padImageData({
-    unpaddedImageData: Buffer.from([0xFF, 0x00, 0xFF]),
+    unpaddedImageData: Buffer.from([0xff, 0x00, 0xff]),
     width,
     height
-  })
-  const filename = 'filename-3.bmp'
+  });
+  const filename = "filename-3.bmp";
   await m.createBitmapFile({
     filename,
     imageData,
     width,
     height,
     bitsPerPixel: 24
-  })
-  const filedata = await readFile(filename)
-  const header = m.readBitmapFileHeader(filedata)
-  t.is(54, header.imageDataOffset)
-})
+  });
+  const filedata = await readFile(filename);
+  const header = m.readBitmapFileHeader(filedata);
+  t.is(54, header.imageDataOffset);
+});
 
-test('writes a DIB header', t => {
+test("writes a DIB header", t => {
   const dibHeader = m.dibHeader({
     width: 300,
     height: 400,
     bitsPerPixel: 1,
     bitmapDataSize: 64
-  })
+  });
   // Check a random field in the header, like the width field.
-  t.is(
-    300,
-    dibHeader.readInt16LE(4)
-  )
-})
+  t.is(300, dibHeader.readInt16LE(4));
+});
 
-test('reads a bitmap file dib header', async t => {
-  const width = 1
-  const height = 1
+test("reads a bitmap file dib header", async t => {
+  const width = 1;
+  const height = 1;
   const imageData = m.padImageData({
-    unpaddedImageData: Buffer.from([0xFF, 0x00, 0xFF]),
+    unpaddedImageData: Buffer.from([0xff, 0x00, 0xff]),
     width,
     height
-  })
-  const filename = 'filename-4.bmp'
+  });
+  const filename = "filename-4.bmp";
   await m.createBitmapFile({
     filename,
     imageData,
     width,
     height,
     bitsPerPixel: 24
-  })
-  const filedata = await readFile(filename)
-  const dibHeader = m.readDibHeader(filedata)
-  t.is(1, dibHeader.height)
-  t.is(0, dibHeader.numberOfColorsInPalette)
-})
+  });
+  const filedata = await readFile(filename);
+  const dibHeader = m.readDibHeader(filedata);
+  t.is(1, dibHeader.height);
+  t.is(0, dibHeader.numberOfColorsInPalette);
+});
 
-test('creates a 1x1 bitmap file', async t => {
-  const width = 1
-  const height = 1
+test("creates a 1x1 bitmap file", async t => {
+  const width = 1;
+  const height = 1;
   const imageData = m.padImageData({
-    unpaddedImageData: Buffer.from([0xFF, 0x00, 0xFF]),
+    unpaddedImageData: Buffer.from([0xff, 0x00, 0xff]),
     width,
     height
-  })
+  });
 
   await m.createBitmapFile({
-    filename: 'filename.bmp',
+    filename: "filename.bmp",
     imageData,
     width,
     height,
     bitsPerPixel: 24
-  })
-  const bitmapFile = await m.readBitmapFile('filename.bmp')
-  t.is(0, bitmapFile.imageData.compare(
-    imageData
-  ))
+  });
+  const bitmapFile = await m.readBitmapFile("filename.bmp");
+  t.is(0, bitmapFile.imageData.compare(imageData));
   // fs.unlinkSync('filename.bmp')
-})
+});
 
-test('creates a 2x2 bitmap file', async t => {
-  const width = 2
-  const height = 2
+test("creates a 2x2 bitmap file", async t => {
+  const width = 2;
+  const height = 2;
   const imageData = m.padImageData({
+    // prettier-ignore
     unpaddedImageData: Buffer.from([
       0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
       0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0xFF
     ]),
     width,
     height
-  })
+  });
 
   await m.createBitmapFile({
-    filename: 'filename-2.bmp',
+    filename: "filename-2.bmp",
     imageData,
     width,
     height,
     bitsPerPixel: 24
-  })
-  const bitmapFile = await m.readBitmapFile('filename-2.bmp')
-  t.is(0, bitmapFile.imageData.compare(
-    imageData
-  ))
+  });
+  const bitmapFile = await m.readBitmapFile("filename-2.bmp");
+  t.is(0, bitmapFile.imageData.compare(imageData));
   // fs.unlinkSync('filename-2.bmp')
-})
+});
 
-test('creates a 2x2 bitmap file with 1 bit per pixel', async t => {
-  const width = 2
-  const height = 2
+test("creates a 2x2 bitmap file with 1 bit per pixel", async t => {
+  const width = 2;
+  const height = 2;
   const imageData = m.padImageData({
-    unpaddedImageData: Buffer.from([
-      0b10000000,
-      0b01000000
-    ]),
+    unpaddedImageData: Buffer.from([0b10000000, 0b01000000]),
     width,
     height
-  })
-  const filename = 'filename-5.bmp'
+  });
+  const filename = "filename-5.bmp";
   await m.createBitmapFile({
     filename,
     imageData,
     width,
     height,
     bitsPerPixel: 1,
+    // prettier-ignore
     colorTable: Buffer.from([
       0xFF, 0x00, 0xFF, 0x00,
       0x00, 0xFF, 0xFF, 0x00
     ])
-  })
-  const bitmapFile = await m.readBitmapFile(filename)
-  console.log(bitmapFile)
-  t.is(0, bitmapFile.imageData.compare(
-    imageData
-  ))
+  });
+  const bitmapFile = await m.readBitmapFile(filename);
+  t.is(0, bitmapFile.imageData.compare(imageData));
   // fs.unlinkSync('filename-2.bmp')
-})
+});
